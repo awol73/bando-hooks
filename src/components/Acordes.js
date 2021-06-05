16d361b3a400b52de0e51b4a590a3ebb4844a0e4
Angel <adelnegro@gmail.com>
2021-06-04 21:01:44 -0300

    Se borraron Bandoneon.js y Acorde.js. Se le sacaron imports innecesarios a Acordes.js

--- src/components/Acordes.js
+++ src/components/Acordes.js
@@ -1,10 +1,6 @@
 import React, { useState, Fragment } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import { Grid } from '@material-ui/core';
+
 
 
 const Acordes = ({acordesDeLaEscala, handleChord}) => {
